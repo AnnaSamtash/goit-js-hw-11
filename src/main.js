5812e95a93c9b1bad1cf7f3cf41831f88165b2eb
AnnaSamtash <sansara6900@gmail.com>
2024-03-08 20:58:41 +0300

    last

--- src/main.js
+++ src/main.js
@@ -3,7 +3,6 @@ import { renderPhotos } from './js/render-functions';
 import SimpleLightbox from "simplelightbox";
 import iziToast from "izitoast";
 
-
 const form = document.querySelector('.search-form');
 export const inputSearch = form.elements.search;
 export const listOfPhotos = document.querySelector('.gallery');
@@ -24,44 +23,45 @@ const handleLoad = () => {
     document.body.classList.add('loaded');
     document.body.classList.remove('loaded_hiding');
 };
-window.onload = handleLoad;
 
 form.addEventListener('submit', sendForm);
 
 function sendForm(evt) {
     evt.preventDefault();
     listOfPhotos.innerHTML = "";
-    showLoader();
+    
     const input = evt.target.elements.search.value.trim();
     if (input !== '') {
-        
-        fetchPhotoFromPixabay()
-            .then((photos) => {
-                renderPhotos(photos.hits)
-                hideLoader();
-                handleLoad();
-            })
-            .catch((error) => {
-                console.log(error);
-                iziToast.error({
-                    message: 'Sorry, an error occurred while loading. Please try again!',
-                    theme: 'dark',
-                    progressBarColor: '#FFFFFF',
-                    color: '#EF4040',
-                    position: 'topRight',
+        window.onload = () => {
+            fetchPhotoFromPixabay()
+                .then((photos) => {
+                    renderPhotos(photos.hits);
+                    hideLoader();
+                    handleLoad();
+                })
+                .catch((error) => {
+                    console.log(error);
+                    iziToast.error({
+                        message: 'Sorry, an error occurred while loading. Please try again!',
+                        theme: 'dark',
+                        progressBarColor: '#FFFFFF',
+                        color: '#EF4040',
+                        position: 'topRight',
+                    });
                 });
+        }
+        window.onload();
+            form.reset();
+        } else {
+            iziToast.show({
+                message: 'Please complete the field!',
+                theme: 'dark',
+                progressBarColor: '#FFFFFF',
+                color: '#EF4040',
+                position: 'topRight',
             });
-        form.reset();
-    } else {
-        iziToast.show({
-            message: 'Please complete the field!',
-            theme: 'dark',
-            progressBarColor: '#FFFFFF',
-            color: '#EF4040',
-            position: 'topRight',
-        });
+        }
     }
-}
 
 
 
